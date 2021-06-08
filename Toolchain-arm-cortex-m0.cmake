405e39413b8d46c86aee7f1ce954c8d6d3aa9244
Milos Lazic <mlazic95@hotmail.com>
2021-06-08 01:32:26 -0700

    initial commit

---
+++ Toolchain-arm-cortex-m0.cmake
@@ -0,0 +1,21 @@
+set(_toolchain_install_dir /home/mlazic/Development/embedded/toolchain/gcc-arm-none-eabi-10-2020-q4-major)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+
+# Defines the target system.
+set(CMAKE_SYSTEM_NAME      Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# Defines the compiler drivers to use.
+set(CMAKE_C_COMPILER  ${_toolchain_install_dir}/bin/arm-none-eabi-gcc)
+set(CMAK_CXX_COMPILER ${_toolchain_install_dir}/bin/arm-none-eabi-g++)
+
+# Tell CMake where to locate binaries, headers, and packages.
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Set the required GCC/G++ architecture options.
+set(CMAKE_C_FLAGS   "-mthumb -mcpu=cortex-m0 -mfloat-abi=soft")
+set(CMAKE_CXX_FLAGS "-mthumb -mcpu=cortex-m0 -mfloat-abi=soft")
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#ifndef __NO_SYSTEM_INIT
+extern "C" void SystemInit()
+{}
+#endif
+
+int main()
+{
+    for(;;);
+
+    return 0; // suppress compiler warnings
+}
